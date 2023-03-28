5bccf519bdb09474813baa4d340e2b89092dfd5d
Dhruv Rayat <dhruvrayat50@gmail.com>
2023-03-28 22:49:01 +1300

    Update: Got the basic structure of the dashboard done

--- src/components/dashboard/dashboard.tsx
+++ src/components/dashboard/dashboard.tsx
@@ -1,5 +1,44 @@
 import "./dashboard.css";
 
+import { useNavigate } from "react-router-dom";
+
 export default function Dashboard() {
-  return <h1>SUS</h1>;
+  let navigate = useNavigate();
+
+  return (
+    <>
+      {(() => {
+        let type = sessionStorage.getItem("type");
+
+        if (type === "consultant") {
+          return (
+            <>
+              <main className="FRE__Dashboard__Cons">
+                <div className="FRE__Dashboard__Cons__Top">
+                  <div className="FRE__Dashboard__Cons-opps"></div>
+                  <div className="FRE__Dashboard__Cons-curr"></div>
+                  <div className="FRE__Dashboard__Cons-col">
+                    <div className="FRE__Dashboard__Cons-chats"></div>
+                    <div className="FRE__Dashboard__Cons-bal"></div>
+                  </div>
+                </div>
+
+                <div className="FRE__Dashboard__Cons__Bottom">
+                  <div className="FRE__Dashboard__Cons-profile"></div>
+                  <div className="FRE__Dashboard__Cons-notifs"></div>
+                  <div className="FRE__Dashboard__Cons-network"></div>
+                </div>
+              </main>
+            </>
+          );
+        } else if (type === "client") {
+          return <h1>cli</h1>;
+        } else if (type === "freerider") {
+          return <h1>fre</h1>;
+        } else {
+          navigate("/");
+        }
+      })()}
+    </>
+  );
 }
