209db2684db09995ad53f4276a887e53dec3b807
Dhruv Rayat <dhruvrayat50@gmail.com>
2023-03-30 15:01:29 +1300

    Update: Change to the layout and to make it look good

--- src/components/communityNetwork/communityNetwork.tsx
+++ src/components/communityNetwork/communityNetwork.tsx
@@ -3,16 +3,32 @@ import "./communityNetwork.css";
 export default function CommunityNetwork() {
   return (
     <main className="FRE__CommunityNetwork">
-      <div className="FRE__CommunityNetwork-hard_software"></div>
-      <div className="FRE__CommunityNetwork-utility"></div>
-      <div className="FRE__CommunityNetwork-insurance"></div>
-      <div className="FRE__CommunityNetwork-balances"></div>
+      <div className="FRE__CommunityNetwork-hard_software">
+        <h2>Hardware and Software</h2>
+      </div>
+      <div className="FRE__CommunityNetwork-utility">
+        <h2>Utilitys</h2>
+      </div>
+      <div className="FRE__CommunityNetwork-insurance">
+        <h2>Insurance</h2>
+      </div>
+      <div className="FRE__CommunityNetwork-balances">
+        <h2>Balances and Activity</h2>
+      </div>
 
-      <div className="FRE__CommunityNetwork-accomodation"></div>
-      <div className="FRE__CommunityNetwork-energy"></div>
+      <div className="FRE__CommunityNetwork-accomodation">
+        <h2>Accomodation</h2>
+      </div>
+      <div className="FRE__CommunityNetwork-energy">
+        <h2>Energy</h2>
+      </div>
 
-      <div className="FRE__CommunityNetwork-banking"></div>
-      <div className="FRE__CommunityNetwork-education"></div>
+      <div className="FRE__CommunityNetwork-banking">
+        <h2>Banking</h2>
+      </div>
+      <div className="FRE__CommunityNetwork-education">
+        <h2>Education</h2>
+      </div>
     </main>
   );
 }
