e983e81fdb0f0b072ec722a4366be07c9a311fca
Dhruv Rayat <dhruvrayat50@gmail.com>
2023-04-18 11:56:25 +1200

    Feature: Creation of SOW done

--- src/components/statementOfWork/statementOfWork.tsx
+++ src/components/statementOfWork/statementOfWork.tsx
@@ -1,23 +1,26 @@
 import "./statementOfWork.css";
 
-import crypto from "crypto";
 import { useState } from "react";
+import CreateSOWButton from "./CreateSOWButton/createSOWButton";
+import CreateSOWForm from "./CreateSOWForm/createSOWForm";
 
-interface IActiveChat {
+export interface IActiveSOW {
   jobTitle: string | null;
   client: string | null;
   cosultant: string | null;
   tasks: [{}] | null;
+  createSOW: boolean;
 }
 
 let i = 0;
 
 export default function StatementOfWork() {
-  const [activeSow, SetActiveSow] = useState<IActiveChat>({
+  const [activeSow, setActiveSow] = useState<IActiveSOW>({
     jobTitle: null,
     client: null,
     cosultant: null,
     tasks: null,
+    createSOW: false,
   });
 
   let jobs = [
@@ -89,9 +92,13 @@ export default function StatementOfWork() {
       <div className="FRE__Main-StatementOfWork__Cur__Jobs">
         <h2>Current Jobs</h2>
 
-        <StatementOfWorkSwicher data={jobs[0]} SetActiveSow={SetActiveSow} />
+        <StatementOfWorkSwicher data={jobs[0]} setActiveSow={setActiveSow} />
 
-        <StatementOfWorkSwicher data={jobs[1]} SetActiveSow={SetActiveSow} />
+        <StatementOfWorkSwicher data={jobs[1]} setActiveSow={setActiveSow} />
+
+        {sessionStorage.getItem("type") === "client" ? (
+          <CreateSOWButton setActiveSow={setActiveSow} i={i} />
+        ) : null}
       </div>
 
       <StatementOfWorkMiddle
@@ -108,72 +115,85 @@ export default function StatementOfWork() {
 
 function StatementOfWorkMiddle(props: any) {
   return (
-    <div className="FRE__Main-StatementOfWork__Job">
-      {props.isActive === "true" ? (
-        <>
-          <h2>{props.data.jobTitle}</h2>
-          <div className="FRE__Main-StatementOfWork__Job-Data">
-            <ul>
-              <li>
-                <strong>Client Name: </strong> {props.data.client}
-              </li>
-              <li>
-                <strong>Consultant name: </strong> {props.data.cosultant}
-              </li>
-            </ul>
-          </div>
-          <div className="FRE__Main-StatementOfWork__Job-Table">
-            <h3>Job description</h3>
-            <div className="FRE__Main-StatementOFWork__Job-Table__Titles">
-              <div className="FRE__Main-StatementOfWork__Job-Table__Title__NR">
-                <p>NR</p>
+    <>
+      {(() => {
+        if (props.data.createSOW === true) {
+          return <CreateSOWForm />;
+        } else if (props.isActive === "true") {
+          return (
+            <div className="FRE__Main-StatementOfWork__Job">
+              <h2>{props.data.jobTitle}</h2>
+              <div className="FRE__Main-StatementOfWork__Job-Data">
+                <ul>
+                  <li>
+                    <strong>Client Name: </strong> {props.data.client}
+                  </li>
+                  <li>
+                    <strong>Consultant name: </strong> {props.data.cosultant}
+                  </li>
+                </ul>
               </div>
-              <div className="FRE__Main-StatementOfWork__Job-Table__Title__TaskDescripton">
-                <p>Task Description</p>
+              <div className="FRE__Main-StatementOfWork__Job-Table">
+                <h3>Job description</h3>
+                <div className="FRE__Main-StatementOFWork__Job-Table__Titles">
+                  <div className="FRE__Main-StatementOfWork__Job-Table__Title__NR">
+                    <p>NR</p>
+                  </div>
+                  <div className="FRE__Main-StatementOfWork__Job-Table__Title__TaskDescripton">
+                    <p>Task Description</p>
+                  </div>
+                  <div className="FRE__Main-StatementOfWork__Job-Table__Title__Amount">
+                    <p>Amount</p>
+                  </div>
+                  <div className="FRE__Main-StatementOfWork__Job-Table__Title__ConfirmedClient">
+                    <p>Confirmed Client</p>
+                  </div>
+                  <div className="FRE__Main-StatementOfWork__Job-Table__Title__ConfirmedConsultant">
+                    <p>Confirmed Consultant</p>
+                  </div>
+                </div>
+
+                {props.data.tasks.map((x: any) => {
+                  i++;
+                  return (
+                    <StatementOfWorkRow
+                      key={x.id}
+                      id={i}
+                      description={x.description}
+                      amount={x.amount}
+                    />
+                  );
+                })}
               </div>
-              <div className="FRE__Main-StatementOfWork__Job-Table__Title__Amount">
-                <p>Amount</p>
-              </div>
-              <div className="FRE__Main-StatementOfWork__Job-Table__Title__ConfirmedClient">
-                <p>Confirmed Client</p>
-              </div>
-              <div className="FRE__Main-StatementOfWork__Job-Table__Title__ConfirmedConsultant">
-                <p>Confirmed Consultant</p>
+              <div className="FRE__Main-StatementOfWork__Job-Buttons">
+                <button>Send</button>
               </div>
             </div>
-
-            {props.data.tasks.map((x: any) => {
-              i++;
-              return (
-                <StatementOfWorkRow
-                  id={x.id}
-                  description={x.description}
-                  amount={x.amount}
-                />
-              );
-            })}
-          </div>
-          <div className="FRE__Main-StatementOfWork__Job-Buttons">
-            <button>Send</button>
-          </div>
-        </>
-      ) : (
-        <h3>Select a Statement of Work</h3>
-      )}
-    </div>
+          );
+        } else {
+          return (
+            <div className="FRE__Main-StatementOfWork__Job">
+              <h3>Select a Statement of Work</h3>
+            </div>
+          );
+        }
+      })()}
+    </>
   );
 }
 
 function StatementOfWorkSwicher(props: any) {
   return (
     <button
-      className="SattementOfWorkSwicher"
+      className="StatementOfWorkSwitcher"
       onClick={() => {
-        props.SetActiveSow({
+        i = 0;
+        props.setActiveSow({
           jobTitle: props.data.jobTitle,
           client: props.data.client,
           cosultant: props.data.cosultant,
           tasks: props.data.taks,
+          createSOW: false,
         });
       }}
     >
