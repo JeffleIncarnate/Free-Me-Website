014e2c9d5da188d432f4eb2afde1f427deb797de
Dhruv Rayat <dhruvrayat50@gmail.com>
2023-05-01 10:48:36 +1200

    refactor: changed the transactionDeetails.tsx to use 2 conponents instead of 1 component

--- src/components/balances/transactionDetails/transactionDetails.tsx
+++ src/components/balances/transactionDetails/transactionDetails.tsx
@@ -10,39 +10,54 @@ export default function BalancesTransactionDetails({
   return (
     <main className="FRE__Balances__TransactionDetails">
       {transactionDetails !== null ? (
-        <>
-          <h2>Transaction Details</h2>
-
-          <div className="FRE__Balances__TransactionDetails-Flex">
-            <div>
-              <h3>Tile: </h3>
-              <p>{transactionDetails.title}</p>
-            </div>
-            <div>
-              <h3>Type: </h3>
-              <p>{transactionDetails.moneyOut ? "Money Out" : "Money In"}</p>
-            </div>
-            <div>
-              <h3>Date: </h3>
-              <p>{transactionDetails.date}</p>
-            </div>
-            <div>
-              <h3>From: </h3>
-              <p>{transactionDetails.from}</p>
-            </div>
-            <div>
-              <h3>To: </h3>
-              <p>{transactionDetails.to}</p>
-            </div>
-            <div>
-              <h3>Description: </h3>
-              <p>{transactionDetails.description}</p>
-            </div>
-          </div>
-        </>
+        <BalancesTransactionDetailsDisplayData
+          transactionDetails={transactionDetails}
+        />
       ) : (
-        <p>Br</p>
+        <BalancesTransactionDetailsDisplaySelect />
       )}
     </main>
   );
 }
+
+export function BalancesTransactionDetailsDisplayData({
+  transactionDetails,
+}: {
+  [key: string]: ITransactionDetails | null;
+}) {
+  return (
+    <>
+      <h2>Transaction Details</h2>
+
+      <div className="FRE__Balances__TransactionDetails-Flex">
+        <div>
+          <h3>Tile: </h3>
+          <p>{transactionDetails!.title}</p>
+        </div>
+        <div>
+          <h3>Type: </h3>
+          <p>{transactionDetails!.moneyOut ? "Money Out" : "Money In"}</p>
+        </div>
+        <div>
+          <h3>Date: </h3>
+          <p>{transactionDetails!.date}</p>
+        </div>
+        <div>
+          <h3>From: </h3>
+          <p>{transactionDetails!.from}</p>
+        </div>
+        <div>
+          <h3>To: </h3>
+          <p>{transactionDetails!.to}</p>
+        </div>
+        <div>
+          <h3>Description: </h3>
+          <p>{transactionDetails!.description}</p>
+        </div>
+      </div>
+    </>
+  );
+}
+let BalancesTransactionDetailsDisplaySelect = () => (
+  <h2>Please select a transaction</h2>
+);
