11bd7ae914c0a67c7868fbd97abda4fd6b248a64
Dhruv Rayat <dhruvrayat50@gmail.com>
2023-05-12 12:06:49 +1200

    feat: Update advertisement to be pb tech

--- src/components/watchlist/advertisment/advertisment.tsx
+++ src/components/watchlist/advertisment/advertisment.tsx
@@ -1,5 +1,13 @@
 import "./advertisment.css";
 
 export default function WatchlistAdvertisment() {
-  return <div className="FRE__Watchlist__Advertisment"></div>;
+  return (
+    <div
+      className="FRE__Watchlist__Advertisment"
+      onClick={() => {
+        window.open("https://www.pbtech.co.nz/", "_blank");
+      }}
+      title="PB Tech"
+    ></div>
+  );
 }
