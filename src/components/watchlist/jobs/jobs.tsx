1dd3c960f23e497142a19ad47933fca51d748e66
Dhruv Rayat <dhruvrayat50@gmail.com>
2023-07-13 20:20:31 +1200

    feat: update

--- src/components/watchlist/jobs/jobs.tsx
+++ src/components/watchlist/jobs/jobs.tsx
@@ -1,25 +1,49 @@
 import "./jobs.css";
 
 import { IWatchList } from "../watchListInterface";
+import { ISOW } from "../../sow/sowInterface";
 
-export default function WatchlistJobs({
-  jobs,
-}: {
-  [key: string]: IWatchList[];
-}) {
+import { useState, useEffect } from "react";
+import { apiURL } from "../../../core/data";
+
+export default function WatchlistJobs() {
+  const [jobs, setJobs] = useState<ISOW[] | null>(null);
+
+  let getCurrentJobs = () => {
+    var requestOptions: { [key: string]: string } = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      `${apiURL}/freeme/getSpecificStatementOfWork?uuid=${sessionStorage.getItem(
+        "uuid"
+      )}`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        setJobs(result);
+      })
+      .catch((error) => console.log("error", error));
+  };
+
+  useEffect(() => {
+    getCurrentJobs();
+  });
+
   return (
     <div className="FRE__Watchlist__Jobs">
       <h2>Jobs</h2>
 
       <ul>
-        {jobs.map((job: IWatchList) => {
-          return (
-            <WatchListJobsButton
-              companyName={job.companyNane}
-              jobName={job.jobName}
-            />
-          );
-        })}
+        {jobs !== null ? (
+          jobs.map((job: ISOW) => {
+            return <WatchListJobsButton jobName={job.name} />;
+          })
+        ) : (
+          <h3>Loading...</h3>
+        )}
       </ul>
     </div>
   );
