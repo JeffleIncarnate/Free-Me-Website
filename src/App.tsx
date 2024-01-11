559f646e147b2f15136aa3f04d6b1634aa21ef39
Dhruv-Rayat1 <dhruvrayat50@gmail.com>
2024-01-11 13:13:23 +1300

    feat: app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import "./index.scss";
 
+import { Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/navbar/navbar";
+
+import HomePage from "./pages/home";
+
 function App() {
   return (
     <>
-      <div>sadfasdf</div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </>
   );
 }
