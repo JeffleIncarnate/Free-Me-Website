fca982b77b5d5770c8b2dd23c3e3a4ae9b9d912d
Dhruv-Rayat1 <dhruvrayat50@gmail.com>
2024-01-11 13:05:49 +1300

    feat: new html for new splash

---
+++ src/components/splash/splash.tsx
@@ -0,0 +1,57 @@
+import "./splash.scss";
+
+import SplashLogo from "../../assets/splashLogo.svg";
+
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+
+export default function Splash() {
+  const [passwordShown, setPasswordShown] = useState<boolean>(false);
+
+  return (
+    <header className="Splash">
+      <div className="Left">
+        <div className="Left__Inner">
+          <h1>
+            Growth Catalysts: Expert <span>Consultation</span>
+          </h1>
+
+          <form
+            onSubmit={() => {
+              alert("e");
+            }}
+          >
+            <div>
+              <label>Email</label>
+              <input type="text" />
+            </div>
+
+            <div>
+              <label>Password</label>
+              <div>
+                <input type={passwordShown ? "text" : "password"} />
+                <FontAwesomeIcon
+                  className="PasswordIcon"
+                  onClick={() => {
+                    setPasswordShown(!passwordShown);
+                  }}
+                  icon={!passwordShown ? faEye : faEyeSlash}
+                />
+              </div>
+            </div>
+
+            <button type="submit">Sign In</button>
+            <div>
+              Don't have an account? <Link to="/signin">Join Us Now!</Link>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div className="Right">
+        <img src={SplashLogo} alt="" />
+      </div>
+    </header>
+  );
+}
