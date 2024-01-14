85da5a5e2310c81c124601fe734e478fc1f503bc
Dhruv-Rayat1 <dhruvrayat50@gmail.com>
2024-01-14 20:46:10 +1300

    feat: basic splash login done

--- src/components/splash/splash.tsx
+++ src/components/splash/splash.tsx
@@ -2,21 +2,72 @@ import "./splash.scss";
 
 import SplashLogo from "../../assets/splashLogo.svg";
 
+import axios from "axios";
 import { useRef, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
+import { z } from "zod";
+import { useMutation } from "@tanstack/react-query";
+import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
+import { toast } from "react-toastify";
+
+type QueryVariables = {
+  email: string;
+  password: string;
+};
 
 export default function Splash() {
   const [passwordShown, setPasswordShown] = useState<boolean>(false);
 
-  const email = useRef(null);
-  const password = useRef(null);
+  const email = useRef<HTMLInputElement | null>(null);
+  const password = useRef<HTMLInputElement | null>(null);
 
-  console.log(email);
-  console.log(password);
+  const mutate = useMutation({
+    mutationFn: ({ email, password }: QueryVariables) => {
+      return axios.post("http://localhost:3000/v1/api/auth/login", {
+        email: email,
+        password: password,
+      });
+    },
+    onSuccess: (ctx) => {
+      console.log(ctx.data);
+      // do redux shit now probably
+      toast.success("e");
+    },
+    onError: (ctx) => {
+      toast.error(ctx.message);
+    },
+  });
 
-  console.log();
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (email.current === null || password.current === null) {
+      toast.error("Provide an Email or Password");
+      return;
+    }
+
+    if (email.current.value === "" || password.current.value === "") {
+      toast.error("Provide an Email or Password");
+      return;
+    }
+
+    const emailSchema = z.string().email();
+
+    if (!emailSchema.safeParse(email.current.value).success) {
+      toast.error("The Email you provided is invalid");
+      return;
+    }
+
+    const emailData = email.current.value;
+    const passwordData = password.current.value;
+
+    mutate.mutate({
+      email: emailData,
+      password: passwordData,
+    });
+  };
 
   return (
     <header className="Splash">
@@ -26,20 +77,19 @@ export default function Splash() {
             Growth Catalysts: Expert <span>Consultation</span>
           </h1>
 
-          <form
-            onSubmit={() => {
-              alert("e");
-            }}
-          >
+          <form onSubmit={handleLogin}>
             <div>
               <label>Email</label>
-              <input type="text" />
+              <input type="text" ref={email} />
             </div>
 
             <div>
               <label>Password</label>
               <div>
-                <input type={passwordShown ? "text" : "password"} />
+                <input
+                  type={passwordShown ? "text" : "password"}
+                  ref={password}
+                />
                 <FontAwesomeIcon
                   className="PasswordIcon"
                   onClick={() => {
@@ -50,9 +100,20 @@ export default function Splash() {
               </div>
             </div>
 
-            <button type="submit">Sign In</button>
+            <button disabled={mutate.isPending} type="submit">
+              {mutate.isPending ? (
+                <>
+                  <FontAwesomeIcon icon={faCircleNotch} spin /> Loading...
+                </>
+              ) : (
+                "Sign In"
+              )}
+            </button>
             <div>
-              Don't have an account? <Link to="/signin">Join Us Now!</Link>
+              Don't have an account?{" "}
+              <Link className="Link" to="/signin">
+                Join Us Now!
+              </Link>
             </div>
           </form>
         </div>
