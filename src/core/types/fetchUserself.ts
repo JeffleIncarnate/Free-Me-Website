4c4c02b9e3294adba8e5fdd1f2b79eced645fe64
Dhruv-Rayat1 <dhruvrayat50@gmail.com>
2024-01-30 15:50:41 +1300

    feat: types for the user

---
+++ src/core/types/fetchUserself.ts
@@ -0,0 +1,84 @@
+export type Post = {
+  postId: string;
+  postText: string;
+  postImages: string[];
+  likes: string[];
+  userId: string;
+};
+
+export type SuccessTypeClient = {
+  id: string;
+  firstname: string;
+  lastname: string;
+  password: string;
+  email: string;
+  phonenumber: string;
+  type: "CONSULTANT" | "CLIENT" | "FREERIDER";
+  dateOfBirth: string;
+  address: string;
+  nzbn: string;
+  gst: string;
+  role: "ADMIN" | "GENERAL";
+  createdAt: Date;
+  profilePicture: string;
+  banner: string;
+  background: string;
+  followers: string[];
+  following: string[];
+  connections: string[];
+  consultantProfile: {
+    id: number;
+    description: string;
+    location: string;
+    tier: string;
+    values: string[];
+    skills: string[];
+    education: string[];
+    ambitions: string[];
+    awards: string[];
+    hobbies: string[];
+    timeline: object;
+    userId: string;
+  };
+  post: Post[] | null;
+};
+
+export type SuccessTypeConsultant = {
+  id: string;
+  firstname: string;
+  lastname: string;
+  password: string;
+  email: string;
+  phonenumber: string;
+  type: "CONSULTANT" | "CLIENT" | "FREERIDER";
+  dateOfBirth: string;
+  address: string;
+  nzbn: string;
+  gst: string;
+  role: "ADMIN" | "GENERAL";
+  createdAt: Date;
+  profilePicture: string;
+  banner: string;
+  background: string;
+  followers: string[];
+  following: string[];
+  connections: string[];
+  consultantProfile: {
+    id: number;
+    description: string;
+    location: string;
+    tier: string;
+    values: string[];
+    skills: string[];
+    education: string[];
+    ambitions: string[];
+    awards: string[];
+    hobbies: string[];
+    timeline: object;
+    userId: string;
+  };
+  post: Post[] | null;
+};
+
+// Union Type
+export type User = SuccessTypeClient | SuccessTypeConsultant;
